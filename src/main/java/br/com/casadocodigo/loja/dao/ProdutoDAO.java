b1e6abfd781ae1015b687848eabfbce5dbf5dbb6
Rafael L Santos <sunwebdeveloper@gmail.com>
2018-01-18 22:55:45 -0200

    Aula 9

--- src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
+++ src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.dao;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -7,6 +9,7 @@ import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
 import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoDoLivro;
 
 @Repository
 public class ProdutoDAO {
@@ -25,6 +28,16 @@ public class ProdutoDAO {
 	public Produto find(Long id) {
 		TypedQuery<Produto> query = this.em.createQuery("select distinct(p) from Produto p left join fetch p.precos where p.id=:id", Produto.class);
 		query.setParameter("id", id);
+		return query.getSingleResult();
+	}
+	
+	public BigDecimal sumAllPricesWithSameProductType(TipoDoLivro tipo){
+		TypedQuery<BigDecimal> query = this.em.createQuery(
+				"select sum(preco.valor) from Produto p "+
+				" join p.precos preco where preco.tipo=:tipo", BigDecimal.class);
+		
+		query.setParameter("tipo", tipo);
+		
 		return query.getSingleResult();
 	}
 }
